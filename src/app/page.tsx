f87d4c3467067f408453559a7bf8b79abb53eb57
Himanshu Jain <jainh246@gmail.com>
2024-11-10 00:21:27 +0530

    stylise home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { PROJECTS } from "@/core/projects";
 
 export default function Home() {
   return (
-    <div className="flex gap-5">
+    <div className="flex justify-center items-center gap-5 h-screen w-screen bg-slate-900">
       {PROJECTS.map(({ id, url, name }) => (
         <ProjectCard url={url} name={name} key={id} />
       ))}
--- src/core/components/ProjectCard.tsx
+++ src/core/components/ProjectCard.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export const ProjectCard = ({ url, name }: { url: string; name: string }) => {
   return (
     <Link href={url}>
-      <div className="border-2 border-gray-600 p-4 rounded-lg hover:opacity-75 cursor-pointer ">
-        <h2 className="text-xl">{name}</h2>
+      <div className="border-2 border-gray-600 p-4 rounded-lg hover:opacity-75 cursor-pointer bg-slate-700">
+        <h2 className="text-xl text-gray-100">{name}</h2>
       </div>
     </Link>
   );
